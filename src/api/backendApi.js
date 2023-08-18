49f193a7b5b43c3a30942860aac3da25621c415f
AlonS3 <64230372+AlonS3@users.noreply.github.com>
2023-08-18 17:28:38 +0300

    format using prettier

--- src/api/backendApi.js
+++ src/api/backendApi.js
@@ -1,11 +1,9 @@
-import axios from "axios";
-import queryClient from "../shared/react-query/queryClient";
-
-
+import axios from "axios"
+import queryClient from "../shared/react-query/queryClient"
 
 export const backendApi = axios.create({
   baseURL: process.env.REACT_APP_BACKEND_URL,
-});
+})
 
 export const setupResponseInterceptor = (navigate, useLogout) => {
   return backendApi.interceptors.response.use(
@@ -14,26 +12,23 @@ export const setupResponseInterceptor = (navigate, useLogout) => {
       return response
     },
     (error) => {
-      const { response } = error;
-      if (error.code === 'ERR_NETWORK') {
+      const { response } = error
+      if (error.code === "ERR_NETWORK") {
         // connection problem
-        if (queryClient.getQueryData(["networkErrorAlert"]) !== true){
+        if (queryClient.getQueryData(["networkErrorAlert"]) !== true) {
           queryClient.setQueryData(["networkErrorAlert"], true)
         }
-        
-      } else if (error.code === 'ERR_CANCELED') {
+      } else if (error.code === "ERR_CANCELED") {
         // connection canceled
-        
       }
 
-      if (response){
+      if (response) {
         const statusCode = response?.status
         // const responseData = response?.data
         // If the request fails, we check if the status is 401, then redirect to the login page
         if (statusCode === 404) {
           // The requested resource does not exist or has been deleted
-        }
-        else if (statusCode === 401) {
+        } else if (statusCode === 401) {
           // Please login to access this resource
           useLogout()
           navigate("/login")
@@ -44,14 +39,11 @@ export const setupResponseInterceptor = (navigate, useLogout) => {
   )
 }
 
-
-
 const sortObjectMap = {
   Trending: "",
   Popularity: "popularity",
   Date: "dateAdded",
-};
-
+}
 
 export const getMaps = async (signal, category, sort, page, query) => {
   const response = await backendApi.get("/api/public/maps", {
@@ -63,9 +55,9 @@ export const getMaps = async (signal, category, sort, page, query) => {
     },
     withCredentials: true,
     signal,
-  });
-  return response.data;
-};
+  })
+  return response.data
+}
 
 export const getUserStatus = async () => {
   const response = await backendApi.get("/api/public/auth/status", {
@@ -73,9 +65,9 @@ export const getUserStatus = async () => {
       "Cache-Control": "no-cache",
     },
     withCredentials: true,
-  });
-  return response.data;
-};
+  })
+  return response.data
+}
 
 export const getUserProfile = async () => {
   const response = await backendApi.get("/api/profile", {
@@ -83,39 +75,41 @@ export const getUserProfile = async () => {
       "Cache-Control": "no-cache",
     },
     withCredentials: true,
-  });
-  return response.data;
-};
-
+  })
+  return response.data
+}
 
 export const getMapData = async (mapId) => {
   const response = await backendApi.get(`/api/public/map/${mapId}`, {
     withCredentials: true,
-  });
-  return response.data;
-};
+  })
+  return response.data
+}
 
-export const likeMap = async ({functionName,mapId}) => {
-  const response = await backendApi.patch(`/api/map/${functionName}/${mapId}`,{}, {
-    withCredentials: true,
-  });
-  return response.data;
-};
+export const likeMap = async ({ functionName, mapId }) => {
+  const response = await backendApi.patch(
+    `/api/map/${functionName}/${mapId}`,
+    {},
+    {
+      withCredentials: true,
+    }
+  )
+  return response.data
+}
 
 export const addMap = async (formData) => {
-  const formDataObject = new FormData();
+  const formDataObject = new FormData()
 
   for (let key in formData) {
-    if (key === "files"){
-      formData[key].forEach(element => formDataObject.append(key,element.file))
-    }
-    else{
-      formDataObject.append(key, formData[key]);
+    if (key === "files") {
+      formData[key].forEach((element) => formDataObject.append(key, element.file))
+    } else {
+      formDataObject.append(key, formData[key])
     }
   }
-  const response = await backendApi.post('/api/map/',formDataObject, {
-    headers: {'Content-Type': 'multipart/form-data'},
+  const response = await backendApi.post("/api/map/", formDataObject, {
+    headers: { "Content-Type": "multipart/form-data" },
     withCredentials: true,
-  });
-  return response.data;
+  })
+  return response.data
 }
